5d7f7ca8d4e15859b8b8fc94bfd98536cfc37d9c
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-01-22 00:55:13 +0530

    Pre-defined Tools Structure

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import OpenAI from "openai";
 import { createAssistant } from "./openai/createAssistant.js";
 import { createThread } from "./openai/createThread.js";
 import { createRun } from "./openai/createRun.js";
+import { performRun } from "./openai/performRun.js";
 
 async function main() {
     const client = new OpenAI()
@@ -10,6 +11,10 @@ async function main() {
     const assistant = await createAssistant(client);
     const thread = await createThread(client, message);
     const run = await createRun(client, thread, assistant.id);
+    const result = await performRun(run, thread, client);
+
+    console.log(result);
+    
 }
 
 main();
