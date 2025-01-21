143df3b5461dc111b2f7307d4114cfdacad76928
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-01-21 23:07:55 +0530

    Thread Creation Functionality

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import OpenAI from "openai";
 import { createAssistant } from "./openai/createAssistant.js";
+import { createThread } from "./openai/createThread.js";
 
 async function main() {
     const client = new OpenAI()
+    const message = "Hello, AI Agent!";
     
     const assistant = await createAssistant(client);
+    const thread = await createThread(client, message);
 }
 
 main();
